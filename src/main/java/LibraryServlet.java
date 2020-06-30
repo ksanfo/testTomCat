6cb9e8603975f56f00dbb21a516d34a16f216e82
ok <ksanfo@gmail.com>
2020-06-30 15:58:34 +0300

    jdbc

---
+++ src/main/java/LibraryServlet.java
@@ -0,0 +1,39 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+public class LibraryServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+
+        try {
+            Connection conn  = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/java_ee_db", "test","test");
+
+            Statement stmt= conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT  title from books");
+            while (rs.next())
+                pw.println(rs.getString("title"));
+            stmt.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
